fe04572bce9cf0d7e75f71041bd3ac4fa22b9257
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2023-05-19 15:53:55 +0200

    updated

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,9 @@ defined('TYPO3') or die('Access denied.');
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sp_wildtier'] = 'EXT:sp_wildtier/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][700] = 'EXT:sp_wildtier/Resources/Private/Layouts/';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['partialRootPaths'][700] = 'EXT:sp_wildtier/Resources/Private/Partials/';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][700] = 'EXT:sp_wildtier/Resources/Private/Templates/Email/';
 
 /***************
  * PageTS
